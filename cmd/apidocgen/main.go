6ea817e216d3a00df312d5f1164dd43f86df9899
Alexander Trost <galexrt@googlemail.com>
2017-05-24 00:26:58 +0200

    Updated api documentation, TODO

--- cmd/apidocgen/main.go
+++ cmd/apidocgen/main.go
@@ -29,7 +29,7 @@ import (
 const (
 	firstParagraph = `# API Docs
 
-This Document documents the types introduced by the Prometheus Operator to be consumed by users.
+This Document documents the types introduced by the Elasticsearch Operator to be consumed by users.
 
 > Note this document is generated from code comments. When contributing a change to this document please do so by changing the code comments.`
 )
